e5ad11dea9509736fafaf65d481ebfa00ebdd5cd
serchg0george <serchg0george@gmail.com>
2023-03-02 13:38:26 +0200

    Homework 13 switch/case optimize code

--- src/homework/lesson13/Main.java
+++ src/homework/lesson13/Main.java
@@ -3,6 +3,6 @@ package homework.lesson13;
 public class Main {
     public static void main(String[] args) {
         Month month = new Month();
-        month.setNumberOfMonth(1);
+        month.setNumberOfMonth(14);
     }
 }
--- src/homework/lesson13/Month.java
+++ src/homework/lesson13/Month.java
@@ -3,41 +3,23 @@ package homework.lesson13;
 public class Month {
     void setNumberOfMonth(int numberOfMonth) {
         switch (numberOfMonth) {
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("30 day");
+                break;
             case 1:
-                System.out.println("January, 31 day");
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("31 day");
                 break;
             case 2:
-                System.out.println("February, 28 day");
-                break;
-            case 3:
-                System.out.println("March, 31 day");
-                break;
-            case 4:
-                System.out.println("April, 30 day");
-                break;
-            case 5:
-                System.out.println("May, 31 day");
-                break;
-            case 6:
-                System.out.println("June, 30 day");
-                break;
-            case 7:
-                System.out.println("July, 31 day");
-                break;
-            case 8:
-                System.out.println("August, 31 day");
-                break;
-            case 9:
-                System.out.println("September, 30 day");
-                break;
-            case 10:
-                System.out.println("October, 31 day");
-                break;
-            case 11:
-                System.out.println("November, 30 day");
-                break;
-            case 12:
-                System.out.println("December, 31 day");
+                System.out.println("28 day");
                 break;
             default:
                 System.out.println("Dude, we have just twelve months");
