8ea81e2152ea3e9c8f1e722d91747d9babc95351
serchg0george <serchg0george@gmail.com>
2023-03-03 17:32:21 +0200

    Homework 13: Minor code changes for readable

--- src/homework/lesson13/Main.java
+++ src/homework/lesson13/Main.java
@@ -3,6 +3,6 @@ package homework.lesson13;
 public class Main {
     public static void main(String[] args) {
         Month month = new Month();
-        month.setNumberOfMonth(14);
+        month.setNumberOfMonth(12);
     }
 }
