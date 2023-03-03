cafa0434ca15237d144a5a15cc235204e7771e8f
serchg0george <serchg0george@gmail.com>
2023-03-03 13:44:22 +0200

    Homework 14 Minor fixes

--- src/homework/lesson14/ShowTime.java
+++ src/homework/lesson14/ShowTime.java
@@ -2,18 +2,14 @@ package homework.lesson14;
 
 public class ShowTime {
     public static void showTime() {
-        for (int hours = 0; hours <= 23; hours++) {
-            if (hours > 1) {
-                System.out.println("The end");
-                break;
-            }
-            for (int minutes = 0; minutes <= 59; minutes++) {
-                if (minutes % 10 == 0 && minutes != 0) {
+        for (int hours = 0; hours < 6; hours++) {
+            for (int minutes = 0; minutes < 60; minutes++) {
+                if ( hours > 1 && minutes % 10 == 0) {
                     break;
                 }
-                for (int seconds = 0; seconds <= 59; seconds++) {
-                    if (seconds*hours > minutes) {
-                        break;
+                for (int seconds = 0; seconds < 60; seconds++) {
+                    if (seconds * hours > minutes) {
+                        continue;
                     }
                     System.out.println("Time is " + hours + " : " + minutes + " : " + seconds);
                 }
