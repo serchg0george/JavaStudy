e83b5619114441d6aa92b37c47f1112c926450f1
serchg0george <serchg0george@gmail.com>
2023-05-30 15:39:03 +0300

    Practice with bubble sorting.

--- src/practiceAndRepetition/sortingPractice/BubbleSorting.java
+++ src/practiceAndRepetition/sortingPractice/BubbleSorting.java
@@ -2,17 +2,18 @@ package practiceAndRepetition.sortingPractice;
 
 public class BubbleSorting {
     public static void bubbleSorting(int[] array) {
-        int n = array.length;
+        int arrayLength = array.length;
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+        for (int i = 0; i < arrayLength - 1; i++) {
+            for (int j = 0; j < arrayLength - i - 1; j++) {
                 if (array[j] > array[j + 1]){
-                    int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
                 }
             }
         }
+
     }
 
     public static void main(String[] args) {
