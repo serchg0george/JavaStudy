dec1fb02f02116b9f6c37134a3224a5c4755236d
serchg0george <serchg0george@gmail.com>
2023-05-30 21:55:55 +0300

    Practice with: LinkedList Stack Queue

---
+++ src/practiceAndRepetition/dataStructurePractice/LinkedListLinky.java
@@ -0,0 +1,69 @@
+package practiceAndRepetition.dataStructurePractice;
+
+public class LinkedListLinky<T> {
+    private Node<T> head;
+    private int size;
+
+    public LinkedListLinky() {
+        head = null;
+        size = 0;
+    }
+
+    public void addToLinkedList(T element) {
+        Node<T> newNode = new Node<>(element);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void removeFromLinkedList(T element) {
+        if (isEmpty()) {
+            return;
+        }
+
+        if (head.data.equals(element)) {
+            head = head.next;
+            size--;
+            return;
+        }
+
+        Node<T> current = head;
+        while (current.next != null) {
+            if (current.next.data.equals(element)) {
+                current.next = current.next.next;
+                size--;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void iterate() {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+        }
+    }
+}
---
+++ src/practiceAndRepetition/dataStructurePractice/StartProgram.java
@@ -0,0 +1,67 @@
+package practiceAndRepetition.dataStructurePractice;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.LinkedList;
+
+public class StartProgram {
+    public static void main(String[] args) {
+        StartProgram startProgram = new StartProgram();
+
+        startProgram.addToLinkedList();
+
+        startProgram.printStack();
+
+        startProgram.printQueue();
+    }
+
+    public void addToLinkedList(){
+        LinkedListLinky<String> linkedList = new LinkedListLinky<>();
+        Scanner in = new Scanner(System.in);
+        String insertToLinkedList;
+
+        while (true){
+            System.out.println("Enter string (or 'n' to exit): ");
+            insertToLinkedList = in.nextLine();
+
+            if (insertToLinkedList.equals("n")){
+                break;
+            }
+            linkedList.addToLinkedList(insertToLinkedList);
+        }
+        linkedList.iterate();
+    }
+
+    public void printStack() {
+        Stack<String> stringStack = new Stack<>();
+        stringStack.push("First added element");
+        stringStack.push("Second added element");
+        stringStack.push("Third added element");
+        stringStack.push("Fourth added element");
+        stringStack.push("Fifth added element");
+
+        while(!stringStack.isEmpty()){
+            String elements = stringStack.pop();
+            System.out.println(elements);
+        }
+        System.out.println(stringStack.empty());
+    }
+
+    public void printQueue() {
+
+        Queue<String> stringQueue = new LinkedList<>();
+        stringQueue.add("First added element");
+        stringQueue.add("Second added element");
+        stringQueue.add("Third added element");
+        stringQueue.add("Fourth added element");
+        stringQueue.add("Fifth added element");
+
+        while(!stringQueue.isEmpty()){
+            String elements = stringQueue.peek();
+            System.out.println(elements);
+            stringQueue.remove();
+        }
+        System.out.println(stringQueue.isEmpty());
+    }
+}
