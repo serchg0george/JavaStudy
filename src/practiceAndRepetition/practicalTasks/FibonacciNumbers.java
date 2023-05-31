a886c7f86085493f744d4bd29474aab8be733af1
serchg0george <serchg0george@gmail.com>
2023-05-31 20:30:21 +0300

    Fibonacci numbers task

---
+++ src/practiceAndRepetition/practicalTasks/FibonacciNumbers.java
@@ -0,0 +1,17 @@
+package practiceAndRepetition.practicalTasks;
+
+public class FibonacciNumbers {
+    public static void main(String[] args) {
+        int n0 = 1; //first number
+        int n1 = 1; //second number
+        int n2; //sum of numbers
+        System.out.print(n0 + " " + n1 + " ");
+        for (int i = 3; i <= 11; i++) {
+            n2 = n0 + n1;
+            System.out.print(n2 + " ");
+            n0 = n1;
+            n1 = n2;
+        }
+        System.out.println();
+    }
+}
