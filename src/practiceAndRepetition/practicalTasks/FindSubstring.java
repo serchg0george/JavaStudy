767e1dbac2be0f60e6c59fe1174ada74f21e1e89
serchg0george <serchg0george@gmail.com>
2023-05-31 20:30:37 +0300

    Find substring in string task

---
+++ src/practiceAndRepetition/practicalTasks/FindSubstring.java
@@ -0,0 +1,10 @@
+package practiceAndRepetition.practicalTasks;
+
+public class FindSubstring {
+    public static void main(String[] args) {
+        String string = "geeksforgeeks";
+        int index = string.indexOf("geeks");
+
+        System.out.println(index);
+    }
+}
