e5fb052b36fec69f551bb6d84afb45cb2039d518
serchg0george <serchg0george@gmail.com>
2023-06-01 22:25:17 +0300

    Are two strings anagram task: The program checked are two strings of anagram or no?

---
+++ src/practiceAndRepetition/practicalTasks/AnagramChecker.java
@@ -0,0 +1,22 @@
+package practiceAndRepetition.practicalTasks;
+
+import java.util.Arrays;
+
+public class AnagramChecker {
+    public static void main(String[] args) {
+        String firstWord = "Silent";
+        String secondWord = "Listen";
+
+        char[] firstWordLetters = firstWord.toLowerCase().toCharArray();
+        char[] secondWordLetters = secondWord.toLowerCase().toCharArray();
+
+        Arrays.sort(firstWordLetters);
+        Arrays.sort(secondWordLetters);
+
+        if (Arrays.equals(firstWordLetters, secondWordLetters)) {
+            System.out.println("This is anagram!");
+        } else {
+            System.out.println("This isn't anagram");
+        }
+    }
+}
